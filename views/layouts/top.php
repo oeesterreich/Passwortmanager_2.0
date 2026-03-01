a83c939a727bfcd3d7385798b869f3828239fc2b
janoschwalter <janowalter@tsn.at>
2026-03-01 22:59:46 +0100

    Implementierung von Methoden

---
+++ views/layouts/top.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>Passwortmanager</title>
+
+    <link rel="shortcut icon" href="/php/3_1_Passwortmanager/css/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/php/3_1_Passwortmanager/css/favicon.ico" type="image/x-icon">
+
+    <link href="/php/3_1_Passwortmanager/css/bootstrap.css" rel="stylesheet">
+    <script src="/php/3_1_Passwortmanager/js/jquery.min.js"></script>
+    <script src="/php/3_1_Passwortmanager/js/bootstrap.min.js"></script>
+    <script src="/php/3_1_Passwortmanager/js/index.js"></script>
+</head>
+<body>
