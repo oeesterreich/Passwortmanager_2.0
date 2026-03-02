b302dffa769f65becfe68bd44e75855a24ee9e39
janoschwalter <janowalter@tsn.at>
2026-03-02 08:19:40 +0100

    Angefangen noch letztes video zu sehen

--- views/credentials/view.php
+++ views/credentials/view.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>Passwortmanager</title>
+
+    <link rel="shortcut icon" href="css/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="css/favicon.ico" type="image/x-icon">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
 <div class="container">
     <h2>Zugangsdaten anzeigen</h2>
 
